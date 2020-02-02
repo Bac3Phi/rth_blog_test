2589ca5580ef89ca601d9aa744f2975dd1ea5875
Bac3Fi <gkztest1@gmail.com>
2020-02-02 23:24:43 +0700

    Fix Deploy

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ApplicationController < ActionController::API
+  include ActionController::RequestForgeryProtection
   protect_from_forgery with: :null_session
   acts_as_token_authentication_handler_for User
 end
