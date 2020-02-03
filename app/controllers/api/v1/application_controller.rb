15844c4c5b41720bb3f0f20cd8fbb14509e2ad8b
Bac3Phi <gkztest1@gmail.com>
2020-02-03 15:17:31 +0700

    remove incluce

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::ApplicationController < ActionController::API
-  include ActionController::RequestForgeryProtection
   protect_from_forgery with: :null_session
   acts_as_token_authentication_handler_for User
 end
